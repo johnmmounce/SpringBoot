f4bb55657034e0e11416f597673a934f12a0eb3e
johnmmounce <john.mounce@mavs.uta.edu>
2024-12-18 22:47:55 -0600

    springboot REST API that handles HTTP  POST Request

--- springboot-rest-api/src/main/java/net/javaguides/springboot_rest_api/controller/StudentController.java
+++ springboot-rest-api/src/main/java/net/javaguides/springboot_rest_api/controller/StudentController.java
@@ -1,10 +1,8 @@
 package net.javaguides.springboot_rest_api.controller;
 
 import net.javaguides.springboot_rest_api.bean.Student;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -47,6 +45,17 @@ public class StudentController {
     @GetMapping("students/query")
     public Student studentRequestVariable (@RequestParam int id,@RequestParam String firstName, @RequestParam String lastName){
         return new Student(id, firstName, lastName);
+    }
+
+    // springboot REST API that handles HTTP  POST Request
+    // @PostMapping and @RequestBody
+    @PostMapping("students/create")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Student createStudent(@RequestBody Student student){
+        System.out.println(student.getId());
+        System.out.println(student.getFirstName());
+        System.out.println(student.getLastName());
+        return student;
     }
 
 }
